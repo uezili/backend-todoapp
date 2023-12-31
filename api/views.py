dc074b353b4b624389ae0c44b39e30842e6c2fc0
Uezili <weslmaciel@gmail.com>
2023-12-31 11:10:09 -0300

    view of project.

---
+++ api/views.py
@@ -0,0 +1,50 @@
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer, UserSerializer
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_exempt
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny, )
+    
+    def perform_create(self, serializer):
+        hashed_password = make_password(serializer.validated_data['password'])
+        serializer.save(password=hashed_password)
+
+
+class UserDetailsView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+    
+
+# @method_decorator(csrf_exempt, name='dispatch')
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        serializer = UserSerializer(token.user)
+        data = {
+            'token': token.key,
+            'user': serializer.data
+        }
+        return Response(data)
